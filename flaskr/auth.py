300aaf7ad276182145d83707da01a0846a835bb1
Kirtsun <olegkirtsun@gmail.com>
2022-10-10 21:51:09 +0300

    create names

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,3 +8,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/names')
+def names():
+    db = get_db()
+    names_ = db.execute(
+        'SELECT COUNT(DISTINCT artist) FROM tracks'
+    )
+    return names_
+
